f5c7dc88bb1fec21992fce8c3d09de379c6e3c1f
Noa Halaly <halalynoa@gmail.com>
2025-01-21 15:59:33 +0200

    Delete EX3 directory

--- EX3/src/exceptions_extention/CharSetIsEmptyException.java
+++
@@ -1,7 +0,0 @@
-package exceptions_extention;
-
-/**
- *  Exception Class - special exception for cases in which program required to create the
- *  ascii art but current charSet is empty.
- */
-public class CharSetIsEmptyException extends Exception{}
--- EX3/src/exceptions_extention/ResOutOfBoundException.java
+++
@@ -1,6 +0,0 @@
-package exceptions_extention;
-
-/**
- *  Exception Class - special exception for cases in which the required resolution is out of bound.
- */
-public class ResOutOfBoundException extends Exception{}
