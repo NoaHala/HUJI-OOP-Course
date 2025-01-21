e45e635f106ee92dc07829e6c364dbb25c0244ee
Noa Halaly <halalynoa@gmail.com>
2025-01-21 15:17:09 +0200

    Add files via upload

---
+++ EX3/src/exceptions_extention/CharSetIsEmptyException.java
@@ -0,0 +1,7 @@
+package exceptions_extention;
+
+/**
+ *  Exception Class - special exception for cases in which program required to create the
+ *  ascii art but current charSet is empty.
+ */
+public class CharSetIsEmptyException extends Exception{}
---
+++ EX3/src/exceptions_extention/OpenImageException.java
@@ -0,0 +1,7 @@
+package exceptions_extention;
+
+
+/**
+ *  Exception Class - special exception for cases in which the program couldn't open or use the given image.
+ */
+public class OpenImageException extends Exception {}
